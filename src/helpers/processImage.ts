0e4d3ebd34a2a5c27cd4051da19b533e47596415
yungando <joji@ando.ws>
2021-10-01 00:33:45 +0100

    test

--- src/helpers/processImage.ts
+++ src/helpers/processImage.ts
@@ -1,73 +1,72 @@
-async function createCanvas(src?: HTMLImageElement | HTMLCanvasElement): Promise<HTMLCanvasElement> {
-  const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
+async function createCanvas(src?: HTMLImageElement | HTMLCanvasElement): Promise<HTMLCanvasElement>
+{
+	const canvas = document.createElement("canvas");
+	const ctx = canvas.getContext("2d");
 
-  if (src) {
-    canvas.width = src.width;
-    canvas.height = src.height;
-    ctx.drawImage(src, 0, 0);
-  }
+	if (src)
+	{
+		canvas.width = src.width;
+		canvas.height = src.height;
+		ctx.drawImage(src, 0, 0);
+	}
 
-  return canvas;
+	return canvas;
 }
 
-export async function process(image: HTMLImageElement, w: number, h: number, blurPx: number = 24, blurOpacity: number = 80, shadowSize: number = 32, centerScale: number = 0.8) {
+export async function process(image: HTMLImageElement, w: number, h: number, blurPx: number = 80, blurOpacity: number = 80, shadowSize: number = 70, centerScale: number = 0.55)
+{
+	const canvas = await createCanvas();
+	const canvasCtx = canvas.getContext("2d");
+	canvas.width = w;
+	canvas.height = h;
 
-  const canvas = await createCanvas();
-  const canvasCtx = canvas.getContext("2d");
-  canvas.width = w;
-  canvas.height = h;
+	// turn source image into workable canvas
 
-  // turn source image into workable canvas
+	const srcCanvas = await createCanvas(image);
 
-  const srcCanvas = await createCanvas(image);
+	const fgShadow = document.createElement("canvas");
+	const fgShadowCtx = fgShadow.getContext("2d");
 
-  // bg
+	fgShadow.width = srcCanvas.width + shadowSize*2;
+	fgShadow.height = srcCanvas.height + shadowSize*2;
 
-  const bg = await createCanvas(srcCanvas);
-  const bgCtx = bg.getContext("2d");
+	fgShadowCtx.shadowBlur = shadowSize;
+	fgShadowCtx.shadowColor = "black";
 
-  bgCtx.filter = `blur(${blurPx}px) opacity(${blurOpacity}%)`;
-  bgCtx.drawImage(bg, 0, 0);
+	fgShadowCtx.drawImage(srcCanvas, shadowSize, shadowSize);
 
-  // fg
+	// overlay
 
-  const fgShadow = document.createElement("canvas");
-  const fgShadowCtx = fgShadow.getContext("2d");
+	if ((w/h) > 1)
+	{
+		// landscape
 
-  fgShadow.width = srcCanvas.width + shadowSize*2;
-  fgShadow.height = srcCanvas.height + shadowSize*2;
+		// calculate fg centering shift
+		const fgXshift = (canvas.width - (centerScale * canvas.height)) / 2;
+		const fgYshift = (canvas.height - (centerScale * canvas.height)) / 2;
 
-  fgShadowCtx.shadowBlur = shadowSize;
-  fgShadowCtx.shadowColor = "black";
+		canvasCtx.filter = `blur(${blurPx}px) opacity(${blurOpacity}%)`;
+		canvasCtx.drawImage(srcCanvas, 0, -(canvas.width/4), canvas.width, canvas.width);
 
-  fgShadowCtx.drawImage(srcCanvas, shadowSize, shadowSize);
+		canvasCtx.filter = "none";
+		canvasCtx.drawImage(fgShadow, fgXshift, fgYshift, canvas.height * centerScale, canvas.height * centerScale); 
+		}
+		else
+		{
+		// portrait
 
+		const fgXshift = (canvas.width - (centerScale * canvas.width)) / 2;
+		const fgYshift = (canvas.height - (centerScale * canvas.width)) / 2;
 
+		canvasCtx.filter = `blur(${blurPx}px) opacity(${blurOpacity}%)`;
+		canvasCtx.drawImage(srcCanvas, -(canvas.width/2.5), 0, canvas.height, canvas.height);
 
-  // overlay
+		canvasCtx.filter = "none";
+		canvasCtx.drawImage(fgShadow, fgXshift, fgYshift, canvas.width * centerScale, canvas.width * centerScale); 
+	}
 
-  if ((w/h) > 1) {
-    // landscape
-
-    // calculate fg centering shift
-    const fgXshift = (canvas.width - (centerScale * canvas.height)) / 2;
-    const fgYshift = (canvas.height - (centerScale * canvas.height)) / 2;
-
-    canvasCtx.drawImage(bg, 0, -(canvas.height/4), canvas.width, canvas.width);
-    canvasCtx.drawImage(fgShadow, fgXshift, fgYshift, canvas.height * centerScale, canvas.height * centerScale); 
-  } else {
-    // portrait
-
-    const fgXshift = (canvas.width - (centerScale * canvas.width)) / 2;
-    const fgYshift = (canvas.height - (centerScale * canvas.width)) / 2;
-
-    canvasCtx.drawImage(bg, -(canvas.width/2.5), 0, canvas.height, canvas.height);
-    canvasCtx.drawImage(fgShadow, fgXshift, fgYshift, canvas.width * centerScale, canvas.width * centerScale); 
-  }
-
-  return await new Promise((resolve: (value: Blob) => void) => {
-    canvas.toBlob((blob) => resolve(blob));
-  });
-
+	return await new Promise((resolve: (value: Blob) => void) =>
+	{
+		canvas.toBlob((blob) => resolve(blob));
+	});
 }
